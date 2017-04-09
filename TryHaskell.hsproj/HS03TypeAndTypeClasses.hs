48de72192a7cddb03c48a1c36f8343a1909caacc
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-04-10 01:47:47 +0700

    Type and Typeclasses

---
+++ TryHaskell.hsproj/HS03TypeAndTypeClasses.hs
@@ -0,0 +1,15 @@
+module HS03TypeAndTypeClasses where
+  
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [ c | c <- st, elem c ['A'..'Z']]
+
+
+addThree x y z = z + y + z
+
+factorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+-- TYPE CLASS
+
